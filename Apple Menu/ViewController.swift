fb88d16d2914f42d48451700111f194207336c07
Ilya <iilavr@mail.ru>
2022-09-03 22:24:11 +0300

    Initial Commit

---
+++ Apple Menu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Menu
+//
+//  Created by Илья on 03.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
