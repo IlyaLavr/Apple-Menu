9f930730eef2c67f30b9bf851f934fdae0e5f2b8
Ilya <iilavr@mail.ru>
2022-09-03 22:50:40 +0300

    add-uitableview

--- Apple Menu/Sources/ViewController.swift
+++ Apple Menu/Sources/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Elements
+    
+    private let cellIdentifier = "cell"
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.backgroundColor = .black
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: TableViewCell.idetifier)
+        return tableView
+    }()
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupHierarhy()
+        setupLayout()
+    }
+    
+    //MARK: - Setup
+
+    private func setupHierarhy() {
+        view.addSubview(tableView)
     }
 
-
+    private func setupLayout() {
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.top)
+            make.left.equalTo(view.snp.left)
+            make.right.equalTo(view.snp.right)
+            make.bottom.equalTo(view.snp.bottom)
+        }
+    }
+}
 }
 
