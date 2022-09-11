373f94abb613b863b9d6e024daca97f02c04f117
Ilya <iilavr@mail.ru>
2022-09-11 23:32:36 +0300

    final-edit

--- Apple Menu/Sources/DetailView.swift
+++ Apple Menu/Sources/DetailView.swift
@@ -11,13 +11,22 @@ class DetailView: UIViewController {
     
     // MARK: - Elements
     
-    let image: UIImageView = {
+    var image: UIImageView = {
         let image = UIImageView()
-        image.image = UIImage(named: "apple")
+        image.image = UIImage()
         return image
     }()
     
     // MARK: - Lifecycle
+    
+    init(imageView: UIImageView? = nil) {
+        super.init(nibName: nil, bundle: nil)
+        self.image = imageView ?? UIImageView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
